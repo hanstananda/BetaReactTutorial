d5f286eaa6bf7280d753ae810d31f3d12f0d4449
Hans Tananda <hanstananda@gmail.com>
2023-02-15 01:54:22 +0800

    add sass, use sass to replace sx for grid board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,13 +7,13 @@ import Square from "./Square";
 import { boxSizePx } from "../contexts/GameGrid";
 import { BoardCfgContext } from "../contexts/BoardCfgContext";
 import calculateWinner from "../hooks/calculateWinner";
-import classes from './Board.module.css'
+import classes from "./Board.module.scss";
 
 function Board() {
-  // TODO: Use context for these, extract more functions out 
+  // TODO: Use context for these, extract more functions out
   const BoardConfig = useContext(BoardCfgContext);
-  const rowSize = BoardConfig.rowSize
-  const colSize = BoardConfig.colSize
+  const rowSize = BoardConfig.rowSize;
+  const colSize = BoardConfig.colSize;
   const [currentPlayer, setNextPlayer] = useState<string>("X");
   const [squares, setSquares] = useState<string[][]>(
     [...Array(rowSize)].map((_) => Array(colSize).fill(""))
@@ -58,30 +58,11 @@ function Board() {
       <Box>
         <Grid
           container
-          // className={classes.gameGrid}
-          sx={{
-            "--Grid-borderWidth": "1px",
-            borderTop: "var(--Grid-borderWidth) solid",
-            borderLeft: "var(--Grid-borderWidth) solid",
-            borderColor: "divider",
-            "& > div": {
-              borderBottom: "var(--Grid-borderWidth) solid",
-              borderColor: "divider",
-            },
-            width: boxSizePx * colSize + 2, // Small hack to fix the Grid size XD
-          }}
+          className={classes.rowGrid}
+          sx={{ width: boxSizePx * colSize + 2 }} // Small hack to fix the Grid size XD
         >
           {squares.map((lines, indexY) => (
-            <Grid
-              container
-              spacing={0}
-              sx={{
-                "& > div": {
-                  borderRight: "var(--Grid-borderWidth) solid",
-                  borderColor: "divider",
-                },
-              }}
-            >
+            <Grid container className={classes.colGrid} spacing={0}>
               {lines.map((item, indexX) => (
                 // Key is used to suppress Warning: Each child in a list should have a unique "key" prop.
                 <Grid xs={4} sx={{ height: boxSizePx, width: boxSizePx }}>
